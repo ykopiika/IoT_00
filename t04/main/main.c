ee79f6722a64bfebdd9407190c5c979f419dd6c7
Yurii Kopiika <ykopiika@e1r1p6.unit.ua>
2020-08-14 17:25:55 +0300

    t04 done

---
+++ t04/main/main.c
@@ -0,0 +1,109 @@
+#include "main.h"
+
+
+static void print_error(char *str)
+{
+    printf("Error: %s\n", str);
+    exit(1);
+}
+
+int count_status(int time, _Bool status)
+{
+    int count = 0;
+    while (gpio_get_level(DH11_DATA_PIN) == status){
+        if (count > time)
+            return -1;
+        ets_delay_us(1);
+        count++;
+    }
+    if (count == 0)
+        return -1;
+    return count;
+}
+
+void print_bits(uint8_t n)
+{
+    if (n > 1)
+        print_bits(n/2);
+    char c = ((n % 2)+48);
+    write(1, &c, 1);
+}
+
+//void test_func(void)
+//{
+//    uint32_t bit = 0;
+//
+//    for (int i = 0; i < 4; ++i) {
+//        bit <<= 1;
+//        bit += 1;
+//    }
+//    for (int i = 0; i < 4; ++i) {
+//        bit = bit << 1;
+//        bit += 0;
+//    }
+//    for (int i = 0; i < 4; ++i) {
+//        bit = bit << 1;
+//        bit += 0;
+//    }
+//    printf("hello world\n");
+//    printf("%d\n", bit);
+//    print_bits(bit);
+//    printf("\n");
+//    char c = (((bit >> (8-1)) & 1)+48);
+//    write(1, &c, 1);
+//    write(1, "\n", 1);
+//}
+
+void app_main()
+{
+    int result = 0;
+    uint8_t arr[5];
+    bzero(&arr, sizeof(arr));
+    
+//    test_func();
+    gpio_set_direction(DH11_POWER_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(DH11_DATA_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(DH11_POWER_PIN, 1);
+    gpio_set_level(DH11_DATA_PIN, 1);
+    sleep(3);
+//    ets_delay_us(30000);
+    gpio_set_level(DH11_DATA_PIN, 0);
+    ets_delay_us(18000);
+    gpio_set_level(DH11_DATA_PIN, 1);
+    ets_delay_us(30);
+    gpio_set_direction(DH11_DATA_PIN, GPIO_MODE_INPUT);
+
+    if ((result = count_status(80, 0)) == -1)
+        print_error("STAGE_1 - Sensor response failed");
+    if ((result = count_status(80, 1)) == -1)
+        print_error("STAGE_2 - Sensor response failed");
+
+    for (int i = 1, j = 0; i < 41; i++)
+    {
+        if ((result = count_status(50, 0)) == -1)
+            print_error("STAGE_3 - Sensor response failed");
+        if ((result = count_status(70, 1)) == -1)
+            print_error("STAGE_4 - Sensor response failed");
+        if (result > 28)
+        {
+            arr[j] <<= 1;
+            arr[j] += 1;
+        }
+        else
+            arr[j] <<= 1;
+        if (i % 8 == 0)
+            j++;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        print_bits(arr[i]);
+        write(1, "\n", 1);
+    }
+    if (arr[0] + arr[1] + arr[2] + arr[3] != arr[4])
+        print_error("Invalid checksum");
+    write(1, "\n", 1);
+    printf("%d%% - humidity\n", arr[0]);
+    printf("%dC - temperature\n", arr[2]);
+}
+
+
