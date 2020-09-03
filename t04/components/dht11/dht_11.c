ec45f73d4c24dc2743eea39f5a44ced010f8061a
Yurii Kopiika <ykopiika@e2r4p9.unit.ua>
2020-09-03 14:37:36 +0300

    refactoring && change warm up time for dht11

--- t04/components/dht11/dht_11.c
+++ t04/components/dht11/dht_11.c
@@ -27,7 +27,7 @@ static void call_to_dht11()
     ESP_ERROR_CHECK(gpio_set_direction(DH11_DATA_PIN, GPIO_MODE_OUTPUT));
     ESP_ERROR_CHECK(gpio_set_level(DH11_POWER_PIN, 1));
     ESP_ERROR_CHECK(gpio_set_level(DH11_DATA_PIN, 1));
-    ets_delay_us(3000000);
+    ets_delay_us(2000100);
     ESP_ERROR_CHECK(gpio_set_level(DH11_DATA_PIN, 0));
     ets_delay_us(18000);
     ESP_ERROR_CHECK(gpio_set_level(DH11_DATA_PIN, 1));
@@ -39,35 +39,36 @@ static void call_to_dht11()
         print_error("STAGE_2 - Sensor response failed");
 }
 
+void set_bit(int result, uint8_t *arr, int *j, int i)
+{
+    if (result > 28)
+    {
+        arr[*j] <<= 1U;
+        arr[*j] += 1U;
+    }
+    else
+        arr[*j] <<= 1U;
+    arr[*j]+=0;
+    if (i % 8 == 0)
+        (*j)++;
+}
+
 void get_value_dht11(uint8_t *temperature, uint8_t *humidity)
 {
     int result = 0;
     uint8_t arr[5];
-
-    if (!temperature || !humidity)
-    {
+    if (!temperature || !humidity) {
         printf("Temperature and/or Humidity pointers is NULL!\n");
         return;
     }
     bzero(&arr, sizeof(arr));
     call_to_dht11();
-
-    for (int i = 1, j = 0; i < 41; i++)
-    {
+    for (int i = 1, j = 0; i < 41; i++) {
         if (count_status(50, 0) == -1)
             print_error("STAGE_3 - Sensor response failed");
         if ((result = count_status(70, 1)) == -1)
             print_error("STAGE_4 - Sensor response failed");
-        if (result > 28)
-        {
-            arr[j] <<= 1;
-            arr[j] += 1;
-        }
-        else
-            arr[j] <<= 1;
-        arr[j]+=0;
-        if (i % 8 == 0)
-            j++;
+        set_bit(result, arr, &j, i);
     }
     if (arr[0] + arr[1] + arr[2] + arr[3] != arr[4])
         print_error("Invalid checksum");
--- t04/components/dht11/dht_11.h
+++ t04/components/dht11/dht_11.h
@@ -2,13 +2,10 @@
 #define BUTTON_DHT_11_H
 
 #include <stdio.h>
-//#include <stdlib.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-//#include <driver/dac.h>
-//#include <unistd.h>
 
 #define DH11_DATA_PIN 4
 #define DH11_POWER_PIN 2
