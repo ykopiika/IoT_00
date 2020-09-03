7868f2ae13d64d15ee7d1cb535bdb432f5418e9a
Yurii Kopiika <ykopiika@e2r4p9.unit.ua>
2020-09-03 15:20:30 +0300

    add ESP_ERROR

--- t05/main/main.c
+++ t05/main/main.c
@@ -1,12 +1,5 @@
 #include <string.h>
 #include "driver/uart.h"
-#include "driver/gpio.h"
-
-//static void print_error(char *str)
-//{
-//    printf("Error: %s\n", str);
-//    exit(1);
-//}
 
 void app_main()
 {
@@ -18,8 +11,11 @@ void app_main()
             .stop_bits = UART_STOP_BITS_1,
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
     };
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, 17, 16, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-    uart_driver_install(UART_NUM_1, 1024, 0, 0, NULL, 0);
-    uart_write_bytes(UART_NUM_1, (const char*)test_str, strlen(test_str));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 17, 16,
+                                 UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, 1024, 0, 0, NULL, 0));
+    if (uart_write_bytes(UART_NUM_1,
+                         (const char*)test_str, strlen(test_str)) == -1)
+        ESP_ERROR_CHECK(ESP_FAIL);
 }
